1945deff788c567158dfe0204a830a7e9ce040bd
Ronalds7 <ronalds.slosbergs@gmail.com>
2022-12-27 21:17:57 +0200

    Changes

--- src/41-circle.ts
+++ src/41-circle.ts
@@ -1,3 +1,5 @@
+import { isConstructorDeclaration } from "typescript";
+
 export {};
 
 /**
@@ -11,42 +13,69 @@ export {};
  *  - https://www.youtube.com/watch?v=YOlr79NaAtQ (What is THIS in JavaScript? in 100 seconds)
  */
 
-class Circle {
-     radius: number;
-     area: any;
-     perimeter: any;
-    constructor(radius: number) {
-      this.radius = radius;
-      this.area = this.area;
-      this.perimeter = this.perimeter;
-    }
+// class Circle {
+//      radius: number;
+//      area: any;
+//      perimeter: any;
+//     constructor(radius: number) {
+//       this.radius = radius;
+//       this.area = this.area;
+//       this.perimeter = this.perimeter;
+//     }
 
-Circle(radius: number) {
-  this.radius = radius;
-  this.area = () => {
-       return (this.radius * this.radius) * 3.14159;
-     
-  };
-  this.perimeter = () => {
-       return this.radius * 3.14159 * 2;
-       }; 
+// Circle(radius: number) {
+//   this.radius = radius;
+//   this.area = () => {
+//        return (this.radius * this.radius) * 3.14159;
+//        };
+//   this.perimeter = () => {
+//        return this.radius * 3.14159 * 2;
+//        }; 
+// }
+// }
 
-}
-}
-
-const c = new Circle(3) ;
-
-console.log("Area =", c.area()); // Expected output: Area = 28.27
-console.log("Perimeter =", c.perimeter()); // Expected output: Perimeter = 18.85
-
+// const c = new Circle(3) ;
+// console.log("Area =", c.area()); // Expected output: Area = 28.27
+// console.log("Perimeter =", c.perimeter()); // Expected output: Perimeter = 18.85
 
 /**
  * This was the last exercise in this repository.
  * Congratulations on getting this far!
  */
 
+// class Circle {
+//      constructor(){
+//      this.radius = radius;
+//      }
+          
+function Circle (this: any, radius: number) {
+     this.radius = radius;
+          
+     const perimeterFunction: number = radius * 3.14159 * 2;
+     const areaFunction: number = radius * radius * 3.14159;
+     this.area = function() {
+          console.log(areaFunction)
+          return areaFunction;
+     };
+     this.perimeter = function() {
+          return perimeterFunction;
+     };
+   }
+   
+   const c = new Circle(3);
+   console.log("Area =", c.call.area()); // Expected output: Area = 28.27
+   console.log("Perimeter =", c.call.perimeter()); // Expected output: Perimeter = 18.85
 
-// Sākotnējais uzdevums
+
+
+
+
+
+
+
+
+
+   // Sākotnējais uzdevums
 // function Circle(radius) {
 //      this.radius = radius;
 //      this.area = function() {};
